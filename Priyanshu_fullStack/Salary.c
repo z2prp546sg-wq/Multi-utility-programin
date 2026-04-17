e9584c7c4afd04838be51971060dde00114ead95
z2prp546sg-wq <z2prp546sg@privaterelay.appleid.com>
2026-04-17 18:10:11 +0530

    Add files via upload

---
+++ Priyanshu_fullStack/Salary.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+float salary,Ad,As,Gross;
+
+printf("\nenter salary =");
+scanf("%f",&salary);
+
+printf("\nenter Ad =");
+scanf("%f",&Ad);
+
+printf("\nenter As =");
+scanf("%f",&As);
+
+Gross=(salary+Ad+As);
+printf("full salary is %2f=",Gross);
+
+    return 0;
+
+}
